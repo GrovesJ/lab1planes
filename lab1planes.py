0dc07c831dc15c3fd5923fc4dfee758ff725bb1e
GrovesJ <j.groves@student.rug.nl>
2025-02-08 11:31:13 +0100

    ADS Lab 1 Planes

---
+++ lab1planes.py
@@ -0,0 +1,62 @@
+from queue import Queue # type: ignore
+from stack import Stack
+from linkedlist import LinkedList
+
+RUNWAY_LENGTH = 7
+HANGAR_SIZE = 5
+
+waiting_runway = Queue()
+hangar = Stack()
+
+def checkup(plane) -> bool:
+    needs_repairs = False
+    if needs_repairs:
+        return True
+    else:
+        return False
+
+
+def process_planes():
+    #input
+    while True:
+        user_input = input("Name\tNeeds Repairs?[Yes/No]\n")
+        if user_input == "":
+            return False
+        name, repair_status = user_input.split("\t")
+        
+       
+        
+        if repair_status == "Yes":
+            if not hangar.size() >= HANGAR_SIZE:
+                hangar.push(name)
+            else:
+                clear_runway()
+                hangar_to_runway()
+                hangar.push(name)
+        elif repair_status == "No" and not hangar.size() >= HANGAR_SIZE:
+            waiting_runway.enqueue(name)
+        else:
+            clear_runway()
+            waiting_runway.enqueue(name)
+
+        clear_runway()
+        hangar_to_runway()
+        clear_runway()
+
+        
+
+def clear_runway() -> None:
+    while waiting_runway.size() > 0:
+        print({waiting_runway.dequeue()},"Departing")
+
+
+def hangar_to_runway() -> None:
+    while hangar.size() > 0:
+        if not waiting_runway.size() >= RUNWAY_LENGTH:
+            waiting_runway.enqueue(hangar.pop())
+        else:
+            clear_runway()
+            waiting_runway.enqueue(hangar.pop())
+
+
+process_planes()
